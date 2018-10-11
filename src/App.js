6778c7bc8ba4ba85acd3ae591a63657086e8c37f
Uneeb Agha <uneeb.agha@gmail.com>
2018-10-11 17:56:04 -0400

    added stuff

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      kittyId: null,
+    }
+  }
+
+  _handleId = (event) => {
+    this.setState({
+      kittyId: event.target.value
+    });
+  }
+
+  _handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     let headerText = "Is it rare?";
 
@@ -9,6 +26,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <div>{headerText}</div>
+          <form onSubmit={this._handleSubmit}>
+            <div className="form-group row">
+              <input type="text" className="form-control"
+                value={this.state.kittyId}
+                onChange={this._handleId} placeholder="Kitty id" />
+              <input type="submit" className="btn btn-primary" value="find out" />
+            </div>
+          </form>
         </header>
       </div>
     );
